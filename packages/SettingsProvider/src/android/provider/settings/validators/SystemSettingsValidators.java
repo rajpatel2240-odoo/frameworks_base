32af7cd6d49bb3f3f0d2431274a5ff3be7e8d71a
nadlabak <pavel@doshaska.net>
2014-12-20 23:56:55 +0200

    Forward port 'Swap volume buttons' (1/3)
    [mikeioannina]: Adjust for 5.0 changes
    [bgcngm]: Adjust for 13.0 changes
    [LuK1337]: Adjust for 14.0 changes
    
    Change-Id: I1b45da63b815aa1b3ddf7cda2b7afb0872ab433f

--- packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
@@ -242,5 +242,6 @@ public class SystemSettingsValidators {
         VALIDATORS.put(System.CLICK_PARTIAL_SCREENSHOT, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.STATUS_BAR_BATTERY_STYLE, new InclusiveIntegerRangeValidator(0, 2));
         VALIDATORS.put(System.STATUS_BAR_SHOW_BATTERY_PERCENT, new InclusiveIntegerRangeValidator(0, 2));
+        VALIDATORS.put(System.SWAP_VOLUME_KEYS_ON_ROTATION, BOOLEAN_VALIDATOR);
     }
 }
